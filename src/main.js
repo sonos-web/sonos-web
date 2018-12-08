97cd702a180b289f373b2599a5e0aae9dfc7e2ab
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-12-08 15:38:50 -0700

    Install vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false;
 
