460a845ddb2b06d1df0b83e260c817ce43fae5d6
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-12-13 11:59:43 -0700

    Remove route, no longer necessary

--- server/routes/zones.js
+++ server/routes/zones.js
@@ -3,14 +3,4 @@ const router = require('express').Router();
 module.exports = function Zones(sonosNetwork) {
   this.router = router;
   this.sonosNetwork = sonosNetwork;
-
-  // Retrieve all
-  this.router.get('/', (req, res) => {
-    console.log(this.sonosNetwork.devices);
-    res.status(200).send('hi');
-    // DeviceDiscovery().once('DeviceAvailable', async function (device) {
-    //     let groups = await device.getAllGroups()
-    //     res.status(200).send(parseZoneGroups(groups))
-    // })
-  });
 };
