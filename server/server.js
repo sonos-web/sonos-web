022599ba3505d7eb78f3bd85abd0aff9db63cfc0
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-12-19 12:07:03 -0700

    Add dotenv

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-const app = require('./express')()
-const http = require('http').Server(app)
-const socketio = require('./socketio')
-const Routes = require('./routes')
-const SonosNetwork = require('./sonos')
+require('dotenv').config();
+const app = require('./express')();
+// eslint-disable-next-line import/order
+const http = require('http').Server(app);
+const socketio = require('./socketio');
+const Routes = require('./routes');
+const SonosNetwork = require('./sonos');
 
 
 const io = socketio.listen(http);
-const sonosNetwork = new SonosNetwork(io)
-const routes = new Routes(sonosNetwork)
+const sonosNetwork = new SonosNetwork(io);
+const routes = new Routes(sonosNetwork);
 
-//Connect all our routes to our app
-app.use('/api', routes.router)
+// Connect all our routes to our app
+app.use('/api', routes.router);
 
-const PORT = process.env.PORT || 5051
-http.listen(PORT, function() {
-  console.info(`Server listening on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5051;
+http.listen(PORT, () => {
+  console.info(`Server listening on port ${PORT}`);
+});
