965937ede74227aa7e5be479871e02540e089655
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-12-20 13:53:19 -0700

    Updated to allow detection and change of volume/mute for each speaker

--- server/sonos/index.js
+++ server/sonos/index.js
@@ -17,7 +17,7 @@ const SonosNetwork = function SonosNetwork(socketio, timeout = 5000) {
           if (error.message === 'No Devices Found') {
             this.socketio.emit('No Sonos Devices Found On Network');
           } else {
-            this.socketio.emit('An Unknown Error Occurred While Retrieving Devices', error);
+            this.socketio.emit('An Unknown Error Occurred While Retrieving Devices', JSON.stringify(error, Object.getOwnPropertyNames(error)));
           }
         });
         // Now that we have the devices, listen for events on them
@@ -33,7 +33,7 @@ const SonosNetwork = function SonosNetwork(socketio, timeout = 5000) {
  * Get Sonos Devices on the network
  * Can we improve this? There should be a way to return quicker once all the devices are found...
  * @param {Number} timeout
- * @returns {Array}
+ * @returns {Array} All devices found on the network
  */
 SonosNetwork.prototype.discover = async function discover() {
   return new Promise((resolve) => {
@@ -85,16 +85,18 @@ SonosNetwork.prototype._listen = function listen() {
         const zoneGroup = this.zoneGroups.find(zg => zg.coordinator.id === device.id);
         if (zoneGroup) {
           this.socketio.emit('Sonos Event Data Received', { groupId: zoneGroup.id, update: transportInfo });
-          this._updateZoneGroup(device.id, transportInfo);
+          this._updateZoneGroupTransportInfo(device.id, transportInfo);
         }
       });
     });
     device.on('RenderingControl', () => {
       this.getRenderingControlInfo(device).then((renderingInfo) => {
-        const zoneGroup = this.zoneGroups.find(zg => zg.coordinator.id === device.id);
+        const zoneGroup = this._zoneGroupForDeviceId(device.id);
         if (zoneGroup) {
-          this.socketio.emit('Sonos Event Data Received', { groupId: zoneGroup.id, update: renderingInfo });
-          this._updateZoneGroup(device.id, renderingInfo);
+          const updatedZone = this._updateZoneGroupRenderingInfo(zoneGroup.id, device.id, renderingInfo);
+          if (updatedZone) {
+            this.socketio.emit('Sonos Event Data Received', { groupId: updatedZone.id, update: updatedZone });
+          }
         }
       });
     });
@@ -105,8 +107,13 @@ SonosNetwork.prototype._listen = function listen() {
       // const zones = JSON.parse(JSON.stringify(zoneData, ' ', 2));
       this._parseZoneGroups().then(() => {
         this.socketio.emit('Sonos Device Discovery Complete', this.zoneGroups);
-      }).catch(() => {
-        this.socketio.emit('No Sonos Devices Found On Network');
+      }).catch((error) => {
+        console.log(error);
+        if (error.message === 'No Devices Found') {
+          this.socketio.emit('No Sonos Devices Found On Network');
+        } else {
+          this.socketio.emit('An Unknown Error Occurred While Retrieving Devices', JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        }
       });
     }
   });
@@ -156,22 +163,105 @@ SonosNetwork.prototype.ungroupAllZones = async function ungroupAllZones() {
   }
 };
 
+/**
+ * Set the volume for a specific zone
+ * @param {String} zoneId
+ * @param {Number} volume
+ */
+SonosNetwork.prototype.setVolume = async function setVolume(zoneId, volume) {
+  const zone = this.devices.find(device => device.id === zoneId);
+  await zone.setVolume(volume);
+};
+
+// SonosNetwork.prototype.setGroupVolume = async function setGroupVolume(groupId, volume) {
+//   const group = this.zoneGroups.find(zg => zg.id === groupId);
+//   const zones = [group.coordinator, ...group.members];
+//   zones.map(async (zone) => {
+//     await zone.device.setVolume(volume);
+//   });
+// };
 
 /**
- * Update(merge) internal zone group with new data
+ * Set mute for a specific zone
+ * @param {String} zoneId
+ * @param {Number} mute
+ */
+SonosNetwork.prototype.setMute = async function setMute(zoneId, mute) {
+  const zone = this.devices.find(device => device.id === zoneId);
+  await zone.setMute(mute);
+};
+
+/**
+ * Update(merge) internal zone group with new AVTransport data
  * @param {String} deviceId
  * @param {Object} data
  */
-SonosNetwork.prototype._updateZoneGroup = function _updateZoneGroup(deviceId, data) {
+// eslint-disable-next-line max-len
+SonosNetwork.prototype._updateZoneGroupTransportInfo = function _updateZoneGroupTransportInfo(deviceId, data) {
   const index = this.zoneGroups.findIndex(group => group.coordinator.id === deviceId);
-  // Merge new data in
-  this.zoneGroups[index] = { ...this.zoneGroups[index], ...data };
+  if (index !== -1) {
+    // Merge new data in
+    this.zoneGroups[index] = { ...this.zoneGroups[index], ...data.transportInfo };
+  }
+};
+
+/**
+ * Returns Updated(merged) internal zone group with new RenderingControl data
+ * @param {String} memberId The device/member id
+ * @param {Object} data
+ * @returns {Object|null} ZoneGroup or null
+ */
+// eslint-disable-next-line max-len
+SonosNetwork.prototype._updateZoneGroupRenderingInfo = function _updateZoneGroupRenderingInfo(groupId, memberId, data) {
+  const index = this.zoneGroups.findIndex(zg => zg.id === groupId);
+  if (index !== -1) {
+    // Merge the data into the coordinator if it matches the given memberId
+    if (this.zoneGroups[index].coordinator.id === memberId) {
+      this.zoneGroups[index].coordinator = { ...this.zoneGroups[index].coordinator, ...data };
+    } else {
+      // otherwise merge it into one of the members
+      const mIndex = this.zoneGroups[index].members.findIndex(member => member.id === memberId);
+      if (mIndex !== -1) {
+        this.zoneGroups[index].members[mIndex] = {
+          ...this.zoneGroups[index].members[mIndex],
+          ...data,
+        };
+      }
+    }
+    // finally, update the zone group mute & volume
+    const renderingInfo = this.getGroupRenderingControlInfo(this.zoneGroups[index]);
+    this.zoneGroups[index] = { ...this.zoneGroups[index], ...renderingInfo };
+    return this.zoneGroups[index];
+  }
+  return null;
+};
+
+/**
+ * Returns group rendering control data based on its members' values
+ * @param {String} group The zoneGroup to query
+ * @returns {Object}
+ */
+// eslint-disable-next-line max-len
+SonosNetwork.prototype.getGroupRenderingControlInfo = function getGroupRenderingControlInfo(group) {
+  const zones = [group.coordinator, ...group.members];
+  // Group will be NOT muted if at least one zone is not muted
+  const mute = !zones.some(zone => !zone.mute);
+  // Average of all volumes
+  // eslint-disable-next-line arrow-body-style
+  const volumeSum = zones.reduce((accumulator, current) => {
+    return (accumulator.volume + current.volume);
+  });
+  // If there is only one element in zones array, it will return the object
+  // otherwise we will have a number in volumeSum
+  const volume = typeof (volumeSum) === 'object' ? volumeSum.volume : Math.floor(volumeSum / zones.length);
+
+  return { volume, mute };
 };
 
 /**
  * Returns RenderingControlInfo
  * @param {Sonos} device
- * @returns {Object}
+ * @returns {Object} RenderingControlInfo
  */
 SonosNetwork.prototype.getRenderingControlInfo = async function getRenderingControlInfo(device) {
   const volume = await device.renderingControlService().GetVolume();
@@ -185,7 +275,7 @@ SonosNetwork.prototype.getRenderingControlInfo = async function getRenderingCont
 /**
  * Returns AVTransportInfo
  * @param {Sonos} device
- * @returns {Object}
+ * @returns {Object} AVTransportInfo
  */
 SonosNetwork.prototype.getAVTransportInfo = async function getAVTransportInfo(device) {
   /**
@@ -276,8 +366,34 @@ SonosNetwork.prototype._parseZoneGroups = async function _parseZoneGroups() {
       // Fetch the transport info for all zones
       await Promise.all(zoneGroups.map(async (group, index) => {
         const transportInfo = await this.getAVTransportInfo(group.coordinator.device);
-        const renderingInfo = await this.getRenderingControlInfo(group.coordinator.device);
-        // merge transportInfo in with zoneGroup
+        // Get rendering info for coordinator & each device in the group
+        zoneGroups[index].coordinator = {
+          ...group.coordinator,
+          ...await this.getRenderingControlInfo(group.coordinator.device),
+        };
+        await Promise.all(group.members.map(async (member, mIndex) => {
+          zoneGroups[index].members[mIndex] = {
+            ...member,
+            ...await this.getRenderingControlInfo(member.device),
+          };
+        }));
+
+
+        // const zones = [group.coordinator, ...group.members];
+        // // Group will be NOT muted if at least one zone is not muted
+        // const mute = !zones.some(zone => !zone.mute);
+        // // Average of all volumes
+        // // eslint-disable-next-line arrow-body-style
+        // const volumeSum = zones.reduce((accumulator, current) => {
+        //   return (accumulator.volume + current.volume);
+        // });
+        // // If there is only one element in zones array, it will return the object
+        // // otherwise we will have a number in volumeSum
+        // const volume = typeof (volumeSum) === 'object' ? volumeSum.volume : volumeSum / zones.length;
+
+        const renderingInfo = this.getGroupRenderingControlInfo(group);
+
+        // merge data in with zoneGroup
         zoneGroups[index] = { ...group, ...transportInfo, ...renderingInfo };
       }));
 
@@ -293,6 +409,22 @@ SonosNetwork.prototype._parseZoneGroups = async function _parseZoneGroups() {
       reject(error);
     });
   });
+};
+
+/**
+ * Returns the zone group that a device belongs to
+ * @param {String} deviceId
+ * @returns {Object|null} Zone Group found for the given deviceId or null
+ */
+SonosNetwork.prototype._zoneGroupForDeviceId = function _zoneGroupForDeviceId(deviceId) {
+  let zoneGroup = this.zoneGroups.find(zg => zg.coordinator.id === deviceId);
+  if (!zoneGroup) {
+    // eslint-disable-next-line arrow-body-style
+    zoneGroup = this.zoneGroups.find((zg) => {
+      return zg.members.findIndex(member => member.device.id === deviceId) > -1;
+    });
+  }
+  return zoneGroup || null;
 };
 
 module.exports = SonosNetwork;
