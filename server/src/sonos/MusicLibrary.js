71db5284d441f8ae1b3a2c030e1f5631174a3f33
Stu Fisher <contact@roygbiv.org.uk>
2021-01-03 11:27:07 +0100

    fix searching

--- server/src/sonos/MusicLibrary.js
+++ server/src/sonos/MusicLibrary.js
@@ -65,7 +65,7 @@ class MusicLibrary {
       // eslint-disable-next-line max-len
       const result = await this.sonos.searchMusicLibrary(category, searchTerm, options.searchOptions, separator);
       if (!result) return MusicLibrary._getEmptyReturnResult();
-      if (options.getAlbumArt) {
+      if (options.getAlbumArt && result.items) {
         switch (options.searchCategory) {
           case 'albumArtists':
           case 'genres':
@@ -117,7 +117,7 @@ class MusicLibrary {
         searchOptions: { start: 0, total },
         search: true,
       });
-      if (result.items.length) {
+      if (result.items && result.items.length) {
         topResults[item] = result;
       }
     }));
@@ -172,6 +172,7 @@ class MusicLibrary {
     };
     const options = { ...defaultOptions, ...searchOptions };
     if (this._sonosPlaylistCache) {
+      if (!this._sonosPlaylistCache.items) return MusicLibrary._getEmptyReturnResult();
       const items = this._sonosPlaylistCache.items.slice(options.start, options.total);
       return {
         total: String(this._sonosPlaylistCache.length),
@@ -183,6 +184,7 @@ class MusicLibrary {
       const playlists = await this.sonos.getMusicLibrary('sonos_playlists');
       if (!playlists) throw new Error(NoResultsFound);
       this._sonosPlaylistCache = playlists;
+      if (!playlists.items) return MusicLibrary._getEmptyReturnResult();
       const items = playlists.items.slice(options.start, options.total);
       return {
         total: String(playlists.length),
@@ -314,7 +316,7 @@ class MusicLibrary {
           searchOptions: { start: 0, total: 1 },
           getAlbumArt: false,
         });
-        [album] = search.items;
+        if (search.items) [album] = search.items;
         if (album && album.albumArtURI !== null) {
           return album.albumArtURI;
         }
